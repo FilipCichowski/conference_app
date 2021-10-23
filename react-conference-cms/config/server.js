c0455b2f3537b3a1611ab08177f91ff69a33e227
FilipCichowski <filip.cichowski.art@gmail.com>
2021-10-23 18:43:15 +0200

    Removed CMS

--- react-conference-cms/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'fc2d76eba36a4323ece4b78fe17745fb'),
-    },
-  },
-});
