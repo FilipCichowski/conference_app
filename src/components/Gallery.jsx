5040e9abb4ef66a940c59a7fce1ba430089e411a
FilipCichowski <filip.cichowski.art@gmail.com>
2021-10-31 14:17:22 +0100

    Non-responsive gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,20 +4,22 @@ import ImageListItem from "@mui/material/ImageListItem";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
 import ListSubheader from "@mui/material/ListSubheader";
 import IconButton from "@mui/material/IconButton";
-import InfoIcon from "@mui/icons-material/Info";
 import axios from "axios";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
 
 class Gallery extends React.Component {
   state = {
     details: [],
     error: null,
+    url: "http://localhost:1337",
   };
 
   componentDidMount = async () => {
     try {
-      const response = await axios.get(
-        "https://janek-filip-conference-app.herokuapp.com/galleries"
-      );
+      const response = await axios.get("http://localhost:1337/galleries");
       this.setState({ details: response.data });
     } catch (error) {
       this.setState({ error });
@@ -25,33 +27,30 @@ class Gallery extends React.Component {
   };
 
   render() {
-    const { error, details } = this.state;
+    const { error, details, url } = this.state;
 
-    this.state.details.map((detail) => console.log(detail.Year));
+    this.state.details.map((detail) => console.log(detail));
 
     return (
-      <ImageList sx={{ width: 500, height: 450 }}>
+      <ImageList variant="masonry" sx={{ mx: "auto", width: "80vw"}}>
         <ImageListItem key="Subheader" cols={2}>
           <ListSubheader component="div">December</ListSubheader>
         </ImageListItem>
         {details.map((item) => (
           <ImageListItem key={item.img}>
             <img
-              src={'https://janek-filip-conference-app.herokuapp.com'+ item.Picture[0].url+'?w=248&fit=crop&auto=format'}
-              srcSet={'https://janek-filip-conference-app.herokuapp.com'+ item.Picture[0].url+'?w=248&fit=crop&auto=format'+'?w=248&fit=crop&auto=format&dpr=2 2x'}
-              alt={item.title}
+              src={`${url}${item.picture.url}?w=100&fit=crop&auto=format`}
+              srcSet={`${url}${item.picture.url}?w=100&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.year}
               loading="lazy"
             />
             <ImageListItemBar
-              title={item.title}
-              subtitle={item.author}
+              title={item.year}
               actionIcon={
                 <IconButton
                   sx={{ color: "rgba(255, 255, 255, 0.54)" }}
-                  aria-label={`info about ${item.title}`}
-                >
-                  <InfoIcon />
-                </IconButton>
+                  aria-label={`info about ${item.year}`}
+                ></IconButton>
               }
             />
           </ImageListItem>
@@ -62,77 +61,3 @@ class Gallery extends React.Component {
 }
 
 export default Gallery;
-
-const itemData = [
-  {
-    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-    title: "Breakfast",
-    author: "@bkristastucchio",
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
-    title: "Burger",
-    author: "@rollelflex_graphy726",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
-    title: "Camera",
-    author: "@helloimnik",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
-    title: "Coffee",
-    author: "@nolanissac",
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
-    title: "Hats",
-    author: "@hjrc33",
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
-    title: "Honey",
-    author: "@arwinneil",
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
-    title: "Basketball",
-    author: "@tjdragotta",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
-    title: "Fern",
-    author: "@katie_wasserman",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
-    title: "Mushrooms",
-    author: "@silverdalex",
-    rows: 2,
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
-    title: "Tomato basil",
-    author: "@shelleypauls",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
-    title: "Sea star",
-    author: "@peterlaster",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
-    title: "Bike",
-    author: "@southside_customs",
-    cols: 2,
-  },
-];
