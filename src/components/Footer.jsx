24d9eee8c2423d9b01ce7ae97dc66538a82bdda1
FilipCichowski <filip.cichowski.art@gmail.com>
2021-11-05 08:40:11 +0100

    Created footer, icons' position need to be fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,30 +3,53 @@ import { Container } from "@material-ui/core";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import GitHubIcon from "@mui/icons-material/GitHub";
-import { flexbox } from "@mui/system";
 import Box from "@mui/material/Box";
-function Footer() {
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = {
+  space_small: {
+    paddingRight: 10,
+    paddingLeft: 10,
+  },
+  textField: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+};
+
+function Footer({ classes }) {
   return (
     <Container
       maxWidth="false"
       style={{
         color: "#FFFFFE",
         backgroundColor: "#030303",
+        height: 35,
       }}
     >
       <Box
-        sx={{ display: "flex", flexDirection: "row", justifyContent: "center" }}
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          height: 35,
+          fontFamily: "Source Sans Pro, sans-serif",
+          fontSize: 20,
+        }}
       >
-        <h6 style={{ paddingLeft: 20, paddingRight: 20 }}>
-          All right reserved
+        <h6 className={classes.textField}>All right reserved</h6>
+        <h6 className={classes.textField}>
+          &copy; Filip Cichowski &#38; Janek Naklicki 2021
         </h6>
-        <h6>&copy; Filip Cichowski &#38; Janek Naklicki 2021</h6>
-        <FacebookIcon></FacebookIcon>
-        <InstagramIcon></InstagramIcon>
-        <GitHubIcon></GitHubIcon>
+        <div>
+          <FacebookIcon className={classes.space_small}></FacebookIcon>
+          <InstagramIcon className={classes.space_small}></InstagramIcon>
+          <GitHubIcon className={classes.space_small}></GitHubIcon>
+        </div>
       </Box>
     </Container>
   );
 }
 
-export default Footer;
+export default withStyles(styles)(Footer);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,8 +20,6 @@ function Home() {
         buttonText={"More details"}
         showButton={true}
       ></HeroUI>
-
-
       <Footer/>
       
     </>
